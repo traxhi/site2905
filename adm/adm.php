2c83026f72f33296558e3918452ef299f93b782c
Sally Lenkulkul <sally_lenkulkul@estudante.sesisenai.org.br>
2023-05-30 08:32:39 -0300

    ii

--- adm/adm.php
+++ adm/adm.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Script Storm - ADM</title>
+    <title>Página do ADM</title>
     <link rel="stylesheet" href="adm.css">
 </head>
 
--- itens&produtos/publicar.php
+++ itens&produtos/publicar.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Publique aqui!</title>
+    <title>Publique sua Obra aqui!</title>
     <link rel="stylesheet" href="publicar.css">
 </head>
 
